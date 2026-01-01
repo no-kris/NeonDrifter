25195fc78785f89f641fc139be89ef5125983d7c
no-kris <kris.tresk@gmail.com>
2025-12-31 19:42:26 -0500

    Added a base entity class that will be a parent to drawing other entities on the screen.

---
+++ js/entities/Entity.js
@@ -0,0 +1,27 @@
+class Entity {
+  constructor(x, y, w = 0, h = 0) {
+    this.x = x;
+    this.y = y;
+    this.width = w;
+    this.height = h;
+    this.vx = 0;
+    this.vy = 0;
+    this.dead = false;
+  }
+
+  update() {
+    this.x += this.vx;
+    this.y += this.vy;
+  }
+
+  draw(ctx, camera) {}
+
+  rectIntersect(obj) {
+    return (
+      this.x < obj.x + obj.w &&
+      this.x + this.width > obj.x &&
+      this.y < obj.y + obj.h &&
+      this.y + this.height > obj.y
+    );
+  }
+}
